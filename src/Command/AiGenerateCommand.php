2697422181b02ba6a698b475e14a2cb24b764a93
Brent Shaffer <betterbrent@google.com>
2025-03-15 12:01:17 -0700

    add ai:generate command

---
+++ src/Command/AiGenerateCommand.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\Question;
+use Symfony\Component\Console\Input\InputOption;
+use GRPC\AiChat\AiChatGrpcClient;
+use GRPC\AiChat\GenerateRequest;
+
+#[AsCommand(
+    name: 'ai:generate',
+    description: 'Calls the gRPC streaming API'
+)]
+class AiGenerateCommand extends Command
+{
+    private $aiChatClient;
+
+    public function __construct(AiChatGrpcClient $aiChatClient)
+    {
+        parent::__construct();
+        $this->aiChatClient = $aiChatClient;
+    }
+
+    protected function configure()
+    {
+        $this
+            ->addOption('message', 'm', InputOption::VALUE_OPTIONAL, 'Message to send to the gRPC server')
+            ->addOption('model', null, InputOption::VALUE_OPTIONAL, 'Model to use for generation', 'gpt2');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $message = $input->getOption('message');
+        $model = $input->getOption('model');
+
+        if (!$message) {
+            $helper = $this->getHelper('question');
+            $question = new Question('Please enter your message: ');
+            $message = $helper->ask($input, $output, $question);
+
+            if (!$message) {
+                $output->writeln('<error>Message cannot be empty.</error>');
+                return Command::FAILURE;
+            }
+        }
+
+        $output->writeln("<info>Sending message to gRPC server: '$message'</info>");
+        $output->writeln("<info>Using model: '$model'</info>");
+
+        $request = new GenerateRequest();
+        $request->setMessage($message);
+        $request->setModel($model);
+
+        $streamingCall = $this->aiChatClient->generate($request);
+
+        $responses = $streamingCall->responses();
+
+        $output->writeln('<info>Responses from gRPC server:</info>');
+        foreach ($responses as $response) {
+            $output->write($response->getResponse());
+        }
+
+        return Command::SUCCESS;
+    }
+}
